a73b68d0470978e9f70ee5a093e4f841e7f6b38b
Jessica Wei <jessicawei9@gmail.com>
2019-08-25 11:07:13 -0700

    game manager set up

--- Rougelike_Tutorial/Assets/Scripts/GameManager.cs
+++ Rougelike_Tutorial/Assets/Scripts/GameManager.cs
@@ -4,13 +4,30 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    // Make GameManager a Singleton (an Object in which there can only be one instance in the game at any given time)
+    public static GameManager instance = null;
+
+
+    public BoardManager boardScript;
+
+    private int level = 3;
+
+    void Awake()
     {
-        
+        if (instance == null)
+            instance = this;
+        else if (instance != this)
+            Destroy(gameObject);
+
+        DontDestroyOnLoad(gameObject); // keeps the GameManager alive even when the scene changes (scene changes destroys everything in the heirarchy otherwise) 
+        boardScript = GetComponent<BoardManager>();
+        InitGame();
     }
 
-    // Update is called once per frame
+    void InitGame() {
+        boardScript.SetupScene(level);
+    }
+
     void Update()
     {
         
